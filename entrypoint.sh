9baa70ac30d70042d2be9a1e0ebcdd0f966f77bf
Damian Duży <dame@zakonfeniksa.org>
2024-10-30 06:29:28 +0100

    Make the entrypoint work on both ubuntu and alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 mkdir -p /etc/thruk/thruk_local.d /var/cache/thruk /var/lib/thruk
-chown -R www-data:www-data /etc/thruk/thruk_local.d /var/cache/thruk /var/lib/thruk
-apache2 -t
-exec apache2 -DFOREGROUND "$@"
+if [ $(grep www-data /etc/passwd) ]; then
+	chown -R www-data:www-data /etc/thruk/thruk_local.d /var/cache/thruk /var/lib/thruk
+	apache2 -t
+	exec apache2 -DFOREGROUND "$@"
+else
+	chown -R apache:apache /etc/thruk/thruk_local.d /var/cache/thruk /var/lib/thruk
+	httpd -t
+	exec httpd -DFOREGROUND "$@"
+fi
